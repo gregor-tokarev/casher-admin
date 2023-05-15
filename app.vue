13a763b86791844178bad1229e9ca252d39354b8
gregor <e@tokarev.work>
2023-05-15 10:19:03 +0300

    add styleguide

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p class="headline-large">some text</p>
   </div>
 </template>
+
+<style lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   ssr: false,
   modules: ["@nuxtjs/stylelint-module", "@pinia/nuxt"],
+  css: ["assets/styles/global.scss"],
+  stylelint: {
+    lintOnStart: false,
+  },
 
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
