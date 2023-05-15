72ebbfcb0b1a5fd089b62a8a5a71998b3d9151f0
gregor <e@tokarev.work>
2023-05-15 08:31:19 +0300

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/stylelint-module"],
+  modules: ["@nuxtjs/stylelint-module", "@pinia/nuxt"],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+  },
 });
