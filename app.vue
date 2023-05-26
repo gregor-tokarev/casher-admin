022ae80d942cbab9d99f0f64abf7c47525aaa7b6
gregor <e@tokarev.work>
2023-05-26 07:54:32 +0300

    Add review card component

--- app.vue
+++ app.vue
@@ -9,11 +9,29 @@
       :page-size="11"
       :max-display-count="10"
     ></ControlsPagination>
+    <CardsProductReview :review="review"></CardsProductReview>
   </div>
 </template>
 
 <script setup lang="ts">
+import { plainToInstance } from "class-transformer";
+import { Review } from "./models/review.model";
+import { User } from "~/models/user.model";
+
 const pValue = ref(3);
+
+const review = ref<Review>(
+  plainToInstance(Review, {
+    id: 234,
+    content: "dsfsdf",
+    score: 3,
+    reviewer: plainToInstance(User, {
+      name: "sfdfs",
+      surname: "sdfsdf",
+      avatarUrl: "https://gravatar.com/avatar/7bcc5de11e4c5a0e092f2478e5f6144e?s=400&d=robohash&r=x",
+    }),
+  })
+);
 </script>
 
 <style lang="scss"></style>
---
+++ components/cards/ProductReview.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { Review } from "~/models/review.model";
+
+interface Props {
+  review: Review;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="card">
+    <div class="card__head">
+      <img
+        :src="review.reviewer.avatarUrl"
+        :alt="review.reviewer.name + ' ' + review.reviewer.surname"
+        class="card__avatar"
+      />
+      <div class="card__head-right">
+        <div class="card__name title-small">{{ review.reviewer.name }} {{ review.reviewer.surname }}</div>
+        <div class="card__reviews">
+          <nuxt-icon v-for="(_, idx) in Array(review.score)" :key="idx" name="star-filled"></nuxt-icon>
+          <nuxt-icon v-for="(_, idx) in Array(5 - review.score)" :key="idx" name="star-outlined"></nuxt-icon>
+        </div>
+      </div>
+    </div>
+    <p class="card__text body-medium">
+      {{ review.content }}
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  padding: 20px;
+  background-color: var(--surface);
+
+  &__head {
+    display: flex;
+    align-items: center;
+  }
+
+  &__head-right {
+    margin-left: 10px;
+  }
+
+  &__avatar {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    overflow: hidden;
+    border-radius: 50%;
+  }
+
+  &__name {
+    color: var(--gray-400);
+  }
+
+  &__reviews {
+    margin-top: 5px;
+    color: var(--accent);
+  }
+
+  &__text {
+    margin-top: 15px;
+    color: var(--gray-400);
+  }
+}
+</style>
